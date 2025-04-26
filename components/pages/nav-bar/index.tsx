205efc2a7a55bab451f00957a135aa0995247616
derana-shamalidilrukshi <shamali@derana.lk>
2025-04-26 11:51:48 +0530

    feat: complete

--- components/pages/nav-bar/index.tsx
+++ components/pages/nav-bar/index.tsx
@@ -27,12 +27,12 @@ export default function Navbar() {
     <header className="w-full bg-white/60 z-50 relative">
       <div className="flex items-center justify-between px-6 py-4">
         {/* Logo */}
-        <Link href="/" className="flex md:items-center space-x-2">
+        <div className="flex md:items-center space-x-2">
           <div className="w-6 h-6 rounded-full bg-gradient-to-r from-yellow-400 to-black flex items-center justify-center text-white">
             <div className="w-1 h-4 bg-white rounded" />
           </div>
-          <span className="text-lg font-semibold text-black">my portfolio</span>
-        </Link>
+          <span className="text-lg font-semibold text-black">My Portfolio</span>
+        </div>
 
         {/* Desktop Nav */}
         <nav className="hidden md:flex items-center gap-8">
